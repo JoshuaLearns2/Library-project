3359971423eec7739fc071d1d78adb2b69eff709
Your Name <jlmeeks587@gmail.com>
2022-10-07 14:26:45 -0400

    Tweaked a few things. Added the delete button for individual cards and a read status toggle switch I'm not too happy with yet.

--- script.js
+++ script.js
@@ -41,16 +41,15 @@ function closeModal(modal) {
 let myLibrary = [];
 
 // Book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages + 'pgs';
-  this.read = read;
 }
 
 // Adds a book to the myLibrary array
-function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages) {
+  let newBook = new Book(title, author, pages);
   myLibrary.splice(0, myLibrary.length);
   myLibrary.push(newBook);
 }
@@ -59,29 +58,78 @@ function addBookToLibrary(title, author, pages, read) {
 let submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
+  if (title.value == false || author.value == false) {
+    document.getElementById('title').placeholder='Required'
+    document.getElementById('author').placeholder='Required'
+    title.classList.add('red')
+    author.classList.add('red')
+  } else {
   addBookToLibrary(title.value, author.value, pages.value)
   displayBooks();
-  document.getElementById('form').reset();
   closeModal(modal)
+  document.getElementById('form').reset();
+  title.placeholder='Title'
+  author.placeholder='Author'
+  clearOtherClasses(title);
+  clearOtherClasses(author);
+  }
+
+  function clearOtherClasses(element) {
+    element.className='';
+  }
+
 });
 
 // Displays books to webpage using DOM manipulation
 function displayBooks() {
   const bookGrid = document.querySelector('.book-grid');
   const card = document.createElement('div');
+  const removeBtn = document.createElement('button')
+
+  const toggleDiv = document.createElement('div')
+  const toggleSwitch = document.createElement('label')
+  const toggleBtn = document.createElement('input')
+  const toggleSlider = document.createElement('span')
+
+
   
   myLibrary.forEach(myLibrary => {
-    const bookGrid = document.querySelector('.book-grid');
-    const card = document.createElement('div');
-    const removeBtn = document.createElement('button')
-
     card.classList.add('card');
     bookGrid.appendChild(card);
     for (let key in myLibrary) {
       const para = document.createElement('p');
       para.textContent = (`${myLibrary[key]}`);  
       card.appendChild(para);
+    };
 
-    }
+    // card.appendChild(toggleBtn)
+
+    toggleDiv.classList.add('ToggleDiv')
+    toggleSwitch.classList.add('switch')
+    toggleBtn.setAttribute('type', 'checkbox')
+    toggleSlider.classList.add('slider-round')
+
+    card.appendChild(toggleDiv)
+    toggleDiv.appendChild(toggleSwitch)
+    toggleSwitch.appendChild(toggleBtn)
+    toggleSwitch.appendChild(toggleSlider)
+
+    removeBtn.classList.add('removeBtn');
+    card.appendChild(removeBtn);
+    removeBtn.innerHTML = "&times;";
+  })
+
+  // function toggleStatus() {
+  //   if (toggleBtn.check == false) {
+  //     toggleBtn.classList.add('unread');
+  //   } else {
+  //     toggleBtn.classList.add('read');
+  //   }
+  // }
+
+  // toggleBtn.addEventListener('click', toggleStatus);
+
+  removeBtn.addEventListener('click', () => {
+    card.remove();
   })
 }
