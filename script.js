4cf18d00923871003ebf3f1d83cbd8edce16d010
Your Name <jlmeeks587@gmail.com>
2022-10-06 23:59:56 -0400

    Added a few small features

--- script.js
+++ script.js
@@ -44,15 +44,15 @@ let myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.pages = pages + 'pgs';
   this.read = read;
 }
 
-// Adds a book to the myLibrary array and displays it on the webpage
+// Adds a book to the myLibrary array
 function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
+  myLibrary.splice(0, myLibrary.length);
   myLibrary.push(newBook);
-  displayBooks();
 }
 
 // Takes values from form and generates a new book
@@ -60,23 +60,28 @@ let submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
   addBookToLibrary(title.value, author.value, pages.value)
+  displayBooks();
+  document.getElementById('form').reset();
   closeModal(modal)
 });
 
 // Displays books to webpage using DOM manipulation
 function displayBooks() {
-  
-  const bookGrid = document.querySelector('.book-grid')
+  const bookGrid = document.querySelector('.book-grid');
+  const card = document.createElement('div');
   
   myLibrary.forEach(myLibrary => {
+    const bookGrid = document.querySelector('.book-grid');
     const card = document.createElement('div');
+    const removeBtn = document.createElement('button')
+
     card.classList.add('card');
     bookGrid.appendChild(card);
     for (let key in myLibrary) {
-      console.log(`${myLibrary[key]}`);
       const para = document.createElement('p');
       para.textContent = (`${myLibrary[key]}`);  
       card.appendChild(para);
+
     }
   })
 }
