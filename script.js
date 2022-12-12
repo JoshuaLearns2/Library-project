271f3726763c9055a538b5267aeac27549cf2583
Your Name <jlmeeks587@gmail.com>
2022-12-12 11:46:17 -0500

    Added some JS functionality to the modal

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('test');
+// Adds functionality to the "Add a book" button by displaying the modal
+
+const addBookBtn = document.querySelector('.add-book-btn')
+const modalBackground = document.querySelector('.modal-background')
+
+addBookBtn.onclick = function() {
+  modalBackground.style.display = "flex";
+}
+
+// Adds functionality to the modal's close button
+
+const closeModal = document.querySelector('.close-modal')
+
+closeModal.onclick = function() {
+  modalBackground.style.display = "none";
+}
+
+// Adds functionality to the modal's submit button by passing the values into the DOM 
+
+const submitBookBtn = document.querySelector('.submit-book-btn')
+
+submitBookBtn.onclick = function(e) {
+  e.preventDefault();
+
+  const cardContainer = document.querySelector('.card-container');
+  const createBookCard = document.createElement('div');
+  createBookCard.className = "card-div";
+  cardContainer.appendChild(createBookCard);
+  modalBackground.style.display = "none";
+
+  const bookTitle = document.createElement('h2');
+  bookTitle.textContent = document.querySelector('.title').value;
+  createBookCard.appendChild(bookTitle);
+  console.log(document.querySelector('.title').value)
+}
