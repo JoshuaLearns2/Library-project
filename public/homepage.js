a9032751e5a593e88a41f3a22354863091e9506f
Joshua <jlmeeks587@gmail.com>
2023-08-25 11:43:32 -0400

    Search functionality for user books

--- public/homepage.js
+++ public/homepage.js
@@ -129,6 +129,8 @@ const renderBookCard = (book) => {
   const bookAuthor = document.createElement('p')
   const cardButtonContainer = document.createElement('div')
   const readStatusButton = document.createElement('button')
+  const favoriteStatusButton = document.createElement('button')
+  const searchButton = document.createElement('button')
   const editButton = document.createElement('button')
   const removeButton = document.createElement('button')
 
@@ -140,10 +142,20 @@ const renderBookCard = (book) => {
     readStatusButton.innerText = 'Unread'
   }
 
+  if (book.favoriteStatus === true) {
+    favoriteStatusButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><path class="favorited" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+  } else if (book.favoriteStatus === false) {
+    favoriteStatusButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><path class="unfavorited" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+  }
+
+  searchButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1.8em" viewBox="0 0 512 512"><path class="search-button" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>'
+
   bookCard.className = 'card-div'
   cardCoverContainer.className = 'card-cover-container'
   coverImage.className = 'book-cover-img'
   cardInfoContainer.className = 'card-info-container'
+  favoriteStatusButton.className = 'favorite-status'
+  searchButton.className = 'search-button'
   cardButtonContainer.className = 'card-btn-container'
   bookTitle.className = 'book-title'
   bookAuthor.className = 'book-author'
@@ -163,11 +175,35 @@ const renderBookCard = (book) => {
   bookCard.appendChild(cardInfoContainer)
   cardInfoContainer.appendChild(bookTitle)
   cardInfoContainer.appendChild(bookAuthor)
+  cardInfoContainer.appendChild(favoriteStatusButton)
+  cardInfoContainer.appendChild(searchButton)
   bookCard.appendChild(cardButtonContainer)
   cardButtonContainer.appendChild(readStatusButton)
   cardButtonContainer.appendChild(editButton)
   cardButtonContainer.appendChild(removeButton)
 }
+
+document.addEventListener('click', async (e) => {
+  if (e.target.matches('.favorite-status')) {
+    let title = e.target.parentElement.parentElement.querySelector('.book-title').innerText
+    let book = await fetchUserBooks().then(books => books.filter(book => book.title === title))
+    if (book[0].favoriteStatus === false) {
+      book[0].favoriteStatus = true
+      e.target.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><path class="favorited" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+    } else if (book[0].favoriteStatus === true) {
+      book[0].favoriteStatus = false
+      e.target.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512"><path class="unfavorited" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+    }
+    editUserBook(book[0])
+  }
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.search-button')) {
+    let title = e.target.parentElement.parentElement.querySelector('.book-title').innerText
+    window.open(`https://www.thriftbooks.com/browse/?b.search=${title.replace(/ /g, '+')}`)
+  }
+})
 
 document.addEventListener('click', async (e) => {
   if (e.target.matches('.unread')) {
